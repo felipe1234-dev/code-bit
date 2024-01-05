3f69f6c4fa8438db5851846ae0af58bd1518991b
Felipe Alves <j.contato.felipe@gmail.com>
2024-01-05 17:15:54 -0300

    add(api/cases/auth/registerUser): add valdiations and code for saving photos in storage

--- src/api/cases/auth/registerUser.ts
+++ src/api/cases/auth/registerUser.ts
@@ -1,14 +1,29 @@
 import { UsersDatabase } from "api/databases";
 import { codes } from "api/constants";
 import { User } from "api/models";
-import { Hash, auth } from "api/utils/services";
+import { Hash, auth, storage } from "api/utils/services";
+import { validateEmail } from "api/utils/functions";
+
+interface UserCustomData extends Omit<Partial<User>, "photo"> {
+    photo?: File;
+}
 
 async function registerUser(
     email: string,
     password: string,
-    customData?: Partial<User>
+    confirmPassword: string,
+    customData: UserCustomData = {}
 ) {
+    const { firstName, lastName, photo: photoFile, ...rest } = customData;
     const usersDB = new UsersDatabase();
+
+    const emailIsValid = validateEmail(email);
+    if (!emailIsValid) throw new Error(codes.auth.invalidEmail);
+    if (!password) throw new Error(codes.auth.passwordRequired);
+    if (password !== confirmPassword)
+        throw new Error(codes.auth.wrongConfirmPassword);
+    if (!firstName) throw new Error(codes.auth.missingFirstName);
+    if (!lastName) throw new Error(codes.auth.missingLastName);
 
     const emailInUse = !!(await usersDB.getByEmail(email));
     if (emailInUse) throw new Error(codes.auth.emailAlreadyTaken);
@@ -23,12 +38,23 @@ async function registerUser(
     if (!authId) throw new Error(codes.auth.unknownError);
 
     const user = new User({
-        ...customData,
+        ...rest,
+        firstName,
+        lastName,
         hashedPassword,
         salt,
         email,
         authId,
     });
+
+    if (photoFile) {
+        const ref = storage.ref(`users/${user.uid}/photos/${photoFile.name}`);
+        await ref.put(photoFile);
+
+        const photoUrl = await ref.getDownloadURL();
+        user.photo = photoUrl;
+    }
+
     await usersDB.uid(user.uid).create(user);
 
     return user;
