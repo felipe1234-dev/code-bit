959d4f1b4b13ae2d304c8b5e183fcc70da245dbc
Felipe Alves <j.contato.felipe@gmail.com>
2024-01-05 17:16:12 -0300

    add(api/constants/codes): new validation codes

--- src/api/constants/codes.ts
+++ src/api/constants/codes.ts
@@ -2,12 +2,17 @@ const codes = {
     auth: {
         unknownError: "UNKNOWN_ERROR" as const,
         emailAlreadyTaken: "EMAIL_ALREADY_TAKEN" as const,
+        invalidEmail: "INVALID_EMAIL" as const,
         wrongEmail: "WRONG_EMAIL" as const,
         wrongPassword: "WRONG_PASSWORD" as const,
+        passwordRequired: "PASSWORD_REQUIRED" as const,
+        wrongConfirmPassword: "WRONG_CONFIRM_PASSWORD" as const,
         missingJwtKey: "PRIVATE_JWT_KEY_NOT_FOUND" as const,
         expiredJwtToken: "JWT_TOKEN_EXPIRED" as const,
         unauthorized: "UNAUTHORIZED" as const,
         unauthenticated: "UNAUTHENTICATED" as const,
+        missingFirstName: "MISSING_FIRST_NAME" as const,
+        missingLastName: "MISSING_LAST_NAME" as const,
     },
     user: {
         notFound: "USER_NOT_FOUND" as const,
