f5c012618a10f7f39e5328447f5e736f1ed12d3b
Felipe Alves <j.contato.felipe@gmail.com>
2024-01-17 17:27:46 -0300

    add(providers/Navigation): create useMemo for getting path params

--- src/providers/Navigation.tsx
+++ src/providers/Navigation.tsx
@@ -3,6 +3,7 @@ import {
     useLocation,
     Location,
     useNavigate,
+    useParams,
     NavigateFunction,
 } from "react-router-dom";
 
@@ -13,6 +14,9 @@ type Route = (typeof routes)[number];
 interface NavigationValue {
     currentLocation: Location;
     currentRoute: Route | undefined;
+    params: {
+        [key: string]: string | undefined;
+    };
     navigate: NavigateFunction;
 }
 
@@ -22,15 +26,60 @@ function NavigationProvider(props: { children: React.ReactNode }) {
     const currentLocation = useLocation();
     const navigate = useNavigate();
 
+    const getPathParts = (path: string) => {
+        return path.split("/").filter(Boolean);
+    };
+
+    const testRoute = (route: Route) => {
+        return getPathParts(route.path).every((part, i) => {
+            const locationParts = getPathParts(currentLocation.pathname);
+            const isPattern = part.includes(":");
+
+            if (isPattern) {
+                const isOptional = part.includes("?");
+
+                if (isOptional) return true;
+                return !!locationParts[i];
+            } else {
+                return part === locationParts[i];
+            }
+        });
+    };
+
     const currentRoute = useMemo(() => {
-        return routes.find((route) => route.path === currentLocation.pathname);
+        const matches = routes.filter(testRoute);
+        return matches[matches.length - 1];
     }, [currentLocation]);
+
+    const params = useMemo(() => {
+        if (!currentLocation || !currentRoute) return {};
+
+        const pathParts = getPathParts(currentLocation.pathname);
+        const params: { [key: string]: string | undefined } = {};
+
+        for (const part of pathParts) {
+            const i = pathParts.indexOf(part);
+            const routePathPart = getPathParts(currentRoute.path)[i];
+
+            const isPattern = routePathPart.includes(":");
+            if (isPattern) {
+                const property = routePathPart
+                    .replace(":", "")
+                    .replace("?", "");
+                const value = part;
+                params[property] = value;
+            }
+        }
+
+        return params;
+    }, [currentLocation, currentRoute]);
 
     return (
         <NavigationContext.Provider
             value={{
                 currentRoute,
                 currentLocation,
+                params,
                 navigate,
             }}
         >
