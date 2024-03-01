305d157829b1b63b5f8546cf1c64872d2b4f908d
Felipe Alves <j.contato.felipe@gmail.com>
2024-03-01 17:08:49 -0300

    add(api/models/Draft): add marker for checking if draft was published

--- src/api/models/Draft.ts
+++ src/api/models/Draft.ts
@@ -3,17 +3,23 @@ import Challenge from "./Challenge";
 
 class Draft extends Challenge {
     public lastSavedAt: Date;
+    public published: boolean;
+    public publishedAt?: Date;
 
     constructor(data: Partial<Draft> = {}) {
         const {
             uid = generateUid("drft-"),
             lastSavedAt = new Date(),
+            published = false,
+            publishedAt,
             ...rest
         } = data;
         super(rest);
-        
+
         this.uid = uid;
         this.lastSavedAt = toDate(lastSavedAt);
+        this.published = !!published;
+        if (publishedAt) this.publishedAt = toDate(publishedAt);
     }
 }
 
